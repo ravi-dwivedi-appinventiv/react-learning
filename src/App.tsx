41c07e5e4659843c02532873fce09536be093a82
Ravi <ravi.dwivedi@appinventiv.com>
2026-08-12 09:54:12 +0530

    crud operation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { useState } from 'react'
-
 import './App.css'
+import ManageUser from './features/users'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
-        <div>
-          <h4>React CI/CD Pipeline</h4>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
+      <ManageUser />
       
     </>
   )
---
+++ src/features/users/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const ManageUser = () => {
+  const [user, setUser] = useState("");
+  const [userList, setUserList] = useState([]);
+
+  const updateUsers = (event) => {
+    const value = event.target.value;
+    setUser(value);
+  };
+
+  const addUser = () => {
+    setUserList((prev) => [...prev, user]);
+    setUser('');
+  };
+
+  const removeUser = (removeIndex) => {
+    setUserList((pre) => pre.filter((_, index) => index != removeIndex));
+  };
+
+  return (
+    <>
+      <div>
+        <h3>User Form</h3>
+        <input type="text" onChange={updateUsers} value={user} />
+        <button onClick={addUser}>Add User</button>
+      </div>
+      <div>
+        {userList?.length > 0 &&
+          userList.map((usr, i) => (
+            <div key={i}>
+              <div>
+                <span>
+                  {" "}
+                  {usr}{" "}
+                  <button onClick={() => removeUser(i)}>Delete</button>{" "}
+                </span>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default ManageUser;
